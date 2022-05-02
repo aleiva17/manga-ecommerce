47f4f191abb1d47cd8014ab363b1bfdc987cb836
aleiva <aleiva1700@gmail.com>
2022-05-02 12:56:50 -0500

    Subir archivos a Firebase

--- src/components/pages/Cart.tsx
+++ src/components/pages/Cart.tsx
@@ -1,9 +1,77 @@
 import CartContext from "../../context/CartContext"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { NavLink } from 'react-router-dom'
+
+import { writeBatch, Timestamp, query, where, getDocs, collection, documentId, addDoc } from "firebase/firestore"
+import { firestoreDb } from "../../services/firebase/index"
 
 const Cart = () => {
   const cartContext = useContext(CartContext)
+  const [showCode, setShowCode] = useState(false)
+  const [stockError, setStockError] = useState(false)
+  const [orderId, setOrderId] = useState("")
+  const [buyerName, setBuyerName] = useState("")
+  const [buyerEmail, setBuyerEmail] = useState("")
+  const [buyerPhone, setBuyerPhone] = useState("")
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBuyerName(e.target.value)
+  }
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBuyerEmail(e.target.value)
+  }
+
+  const handlePhoneChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBuyerPhone(e.target.value)
+  }
+
+  const createOrder = () => {
+    const objectOrder = {
+      buyer: {
+        name: buyerName,
+        email: buyerEmail,
+        phone: buyerPhone
+      },
+      items: cartContext?.cart,
+      date: Timestamp.fromDate(new Date()),
+      total: cartContext?.getTotal()
+    }
+    const ids = cartContext?.cart.map(prod => prod.id)
+    const batch = writeBatch(firestoreDb)
+    const collectionRef = collection(firestoreDb, "products")
+
+    const outOfStock:any[] = []
+
+    getDocs(query(collectionRef, where(documentId(), 'in', ids)))
+      .then(response => {
+        response.docs.forEach(doc => {
+          const dataDoc = doc.data()
+          const prodQuantity = cartContext?.cart.find(prod => prod.id === doc.id)?.quantityToBuy
+
+          if (prodQuantity !== undefined && dataDoc.stock >= prodQuantity) {
+            batch.update(doc.ref, { stock: dataDoc.stock - prodQuantity })
+          }
+          else {
+            outOfStock.push({ id: doc.id, ...dataDoc })
+          }
+        })
+      }).then(() => {
+        if (outOfStock.length === 0) {
+          const collectionRef = collection(firestoreDb, "orders")
+          return addDoc(collectionRef, objectOrder)
+        }
+        else {
+          return Promise.reject({ name: "Out Of Stock", products: outOfStock })
+        }
+      }).then(({ id }) => {
+        batch.commit()
+        setShowCode(true)
+        setOrderId(id)
+      }).catch(error => {
+        setStockError(true)
+      })
+  }
 
   if (cartContext?.cart.length === 0) {
     return (
@@ -80,6 +148,41 @@ const Cart = () => {
       <p className="text-center text-2xl font-bold">
         Total a pagar: <span className="text-green-500">${ cartContext?.getTotal() }</span>
       </p>
+      <div className="flex flex-col gap-4 my-4">
+        <div>
+          <p>Nombre</p>
+          <input onChange={ e => handleNameChange(e) } className="text-2xl rounded-full h-10 w-5/6 md:w-80 px-4 border-2" type="text" />
+        </div>
+        <div>
+          <p>Correo electrónico</p>
+          <input onChange={ e => handleEmailChange(e) } className="text-2xl rounded-full h-10 w-5/6 md:w-80 px-4 border-2" type="text" />
+        </div>
+        <div>
+          <p>Número telefónico</p>
+          <input onChange={ e => handlePhoneChange(e) } className="text-2xl rounded-full h-10 w-5/6 md:w-80 px-4 border-2" type="text" />
+        </div>
+      </div>
+      <div className="flex flex-wrap gap-4">
+        <button 
+          onClick={ () => cartContext?.clear() } 
+          className="transition-colors duration-150 bg-red-500 rounded-lg focus:shadow-outline hover:bg-red-600 px-4 py-2 text-white">
+          Vaciar carrito
+        </button>
+        {
+          (buyerEmail.length !== 0 && buyerName.length !== 0 && buyerPhone.length !== 0) && 
+          <button
+            onClick={ createOrder } 
+            className="transition-colors duration-150 bg-green-500 rounded-lg focus:shadow-outline hover:bg-green-600 px-4 py-2 text-white">
+            Comprar
+          </button>
+        }
+      </div>
+      {
+        showCode && <p className="font-semibold mt-4">Su ID de compra es: <span className="text-green-500">{ orderId }</span></p>
+      }
+      {
+        stockError && <p className="font-semibold text-red-500">Error: Tiene productos fuera de stock. Actualice la página</p>
+      }
     </main>
   )
 }
